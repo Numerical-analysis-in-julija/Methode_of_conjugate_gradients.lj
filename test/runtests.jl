9ce32beb38ab1f2e26914b2c4270187d82fd743b
lovc21 <jakob.dekleva@gmail.com>
2023-04-10 17:53:15 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using Test
 
-@test true
-@test 1 == 1
-@test 1 != 2
-@test 1 < 2
+# Test 1: ScatteredArray multiplication
+function test_scattered_array_multiplication()
+    A = ScatteredArray([1.0 0.0; 0.0 2.0], [1 0; 0 2])
+    x = [1.0, 2.0]
+    expected_result = [1.0, 4.0]
+    @test A * x == expected_result
+end
+
+# Test 2: conj_grad with a simple linear system
+function test_conj_grad()
+    A = ScatteredArray([2.0 1.0; 1.0 3.0], [1 2; 1 2])
+    b = [3.0, 4.0]
+    x, _, _ = conj_grad(A, b)
+    expected_result = [1.0, 1.0]
+    @test isapprox(x, expected_result, atol=1e-6)
+end
+
+# Test 3: conj_grad_2d with a simple 2D optimization problem
+function test_conj_grad_2d()
+    x0 = [2.0, 2.0]
+    sol, _ = conj_grad_2d(grad_f, x0)
+    expected_result = [0.0, 0.0]
+    @test isapprox(sol, expected_result, atol=1e-6)
+end
